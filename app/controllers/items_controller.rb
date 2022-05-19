15521dc4b9bf0a71df9963a1af68c86390d2df1a
Johnathan <jocuog@gmail.com>
2022-05-19 09:03:17 -0600

    commit tree e9feb02f2d4140279d58f94a1b9c5e46f33da521

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,39 @@
 class ItemsController < ApplicationController
-
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id]
+      user = find_user
+      items = user.items
+    else
+      items  = Item.all
+    end
+      render json: items, include: :user
+  end
+
+  def show
+    item = Item.find(params[:id])
+    render json: item, include: :user
+  end
+
+  def create
+    user =find_user
+    item = user.items.create!(item_params)
+    render json: item, include: :user, status: :created
+  end
+
+  private
+
+  def find_user
+    User.find(params[:user_id])
+  end
+
+  def item_params
+    params.permit(:name, :price, :description)
+  end
+
+  def render_not_found_response(exception)
+    render json: { error: "#{exception.model} not found" }, status: :not_found
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,20 @@
 class UsersController < ApplicationController
 
+
   def show
-    user = User.find_by(id: params[:id])
+    user = User.find(params[:id])
     render json: user, include: :items
+  end
+
+  def create
+    user = User.create!(user_params)
+    render json: user, status: :created
+  end
+
+  private
+
+  def user_params
+    params.permit(:username, :city)
   end
 
 end
